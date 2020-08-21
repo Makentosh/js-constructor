a880c2afdc6c6a747df5b5ca1aafc52af2bb452c
Makentosh <ziki_007@ukr.net>
2020-08-21 17:44:02 +0300

    add module

---
+++ src/model.js
@@ -0,0 +1,5 @@
+export const model = [
+  {type: 'title', value: 'Test Title', options: {}},
+  {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, rem!', options: {}},
+  {type: 'textColumns', value: ['1 text', '2 text', '3 text'], options: {}}
+];
